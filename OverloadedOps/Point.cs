2c385965a8e9aab2f1ac2c047a715a24180bd624
GreedNeSS <ruslan199126@gmail.com>
2022-02-05 21:07:57 +0300

    Добавьте файлы проекта.

---
+++ OverloadedOps/Point.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadedOps
+{
+    class Point : IComparable<Point>
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString() => $"[{X}, {Y}]";
+        public override bool Equals(object obj) => obj?.ToString() == this.ToString();
+        public override int GetHashCode() => this.ToString().GetHashCode();
+
+        public int CompareTo(Point other)
+        {
+            if (this.X > other.X && this.Y > other.Y)
+            {
+                return 1;
+            }
+            else if (this.X < other.X && this.Y < other.Y)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static Point operator +(Point point1, Point point2) =>
+            new Point(point1.X + point2.X, point1.Y + point2.Y);
+
+        public static Point operator -(Point point1, Point point2) =>
+            new Point(point1.X - point2.X, point1.Y - point2.Y);
+
+        public static Point operator +(int change, Point point2) =>
+            new Point(change + point2.X, change + point2.Y);
+
+        public static Point operator +(Point point1, int change) =>
+            new Point(change + point1.X, change + point1.Y);
+
+        public static Point operator -(int change, Point point2) =>
+            new Point(change - point2.X, change - point2.Y);
+
+        public static Point operator -(Point point1, int change) =>
+            new Point(point1.X - change, point1.Y - change);
+
+        public static Point operator ++(Point point) =>
+            new Point(point.X + 1, point.Y + 1);
+
+        public static Point operator --(Point point) =>
+            new Point(point.X - 1, point.Y - 1);
+
+        public static bool operator ==(Point point1, Point point2) =>
+            point1.Equals(point2);
+
+        public static bool operator !=(Point point1, Point point2) =>
+            !point1.Equals(point2);
+
+        public static bool operator <(Point p1, Point p2) => p1.CompareTo(p2) < 0;
+
+        public static bool operator >(Point p1, Point p2) => p1.CompareTo(p2) > 0;
+
+        public static bool operator <=(Point p1, Point p2) => p1.CompareTo(p2) <= 0;
+
+        public static bool operator >=(Point p1, Point p2) => p1.CompareTo(p2) >= 0;
+    }
+}
---
+++ OverloadedOps/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace OverloadedOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(100, 100);
+            Point p2 = new Point(40, 40);
+
+            Console.WriteLine($"p1: {p1}");
+            Console.WriteLine($"p2: {p2}");
+            Console.WriteLine();
+
+            Console.WriteLine($"p1 == p2: {p1 == p2}");
+            Console.WriteLine($"p1 != p2: {p1 != p2}");
+            Console.WriteLine();
+
+            Console.WriteLine($"p1 < p2: {p1 < p2}");
+            Console.WriteLine($"p1 > p2: {p1 > p2}");
+            Console.WriteLine();
+
+            Console.WriteLine($"p1 <= p2: {p1 <= p2}");
+            Console.WriteLine($"p1 >= p2: {p1 >= p2}");
+            Console.WriteLine();
+
+            Console.WriteLine($"p1 + p2: {p1 + p2}");
+            Console.WriteLine($"p1 - p2: {p1 - p2}");
+            Console.WriteLine();
+
+            Console.WriteLine($"p1 + 100: {p1 + 100}");
+            Console.WriteLine($"100 + p1: {100 + p1}");
+            Console.WriteLine();
+
+            Console.WriteLine($"p1 - 100: {p1 - 100}");
+            Console.WriteLine($"1000 - p1: {1000 - p1}");
+            Console.WriteLine();
+
+            Point p3 = new Point(90, 5);
+            Console.WriteLine($"p3: {p3}");
+            Console.WriteLine($"p3 += p2: {p3 += p2}");
+            Console.WriteLine();
+
+            Point p4 = new Point(0, 500);
+            Console.WriteLine($"p4: {p4}");
+            Console.WriteLine($"p4 -= p3: {p4 -= p3}");
+            Console.WriteLine();
+
+            Point p5 = new Point(1, 1);
+            Console.WriteLine($"++p5: {++p5}");
+            Console.WriteLine($"--p5: {--p5}");
+            Console.WriteLine();
+
+            Point p6 = new Point(20, 20);
+            Console.WriteLine($"p6++: {p6++}");
+            Console.WriteLine($"p6--: {p6--}");
+            Console.WriteLine();
+        }
+    }
+}
